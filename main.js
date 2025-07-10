c2491606801e17765ce1c6c537e5692440f3a32b
Prajwal Shetty <89895490+pcshetty1@users.noreply.github.com>
2025-07-10 12:06:37 +0530

    Update main.js
    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ document.getElementById("whatsappForm").addEventListener("submit", function (e) 
   const message = document.getElementById("message").value.trim();
 
   const fullMessage = 
-  `New Inquiry from Yoga Bliss Website* 
+  `Namaste! Welcome to YogStudio!
+  New Inquiry from Yoga Bliss Website 
   Name: ${name}
   Email: ${email}
   Message: ${message}
-  Sent via the contact form on our website.Namaste!`;
+  Sent via the contact form on our website.`;
 
   const phoneNumber = "919096543582";
 
